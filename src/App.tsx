17b47e00ecc95de44b0a77ad2e5e30192617e3e4
AzureAD\JemaicaMaeMon <jmon@dunbraesubic.ph>
2023-07-25 08:28:06 +0800

    complete form and table

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 
-import { Box } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 import { MembersTable } from './components/MembersTable'
+import { AddMemberForm } from './components/AddMemberForm'
+import { useState } from 'react';
 function App() {
-
+  const [family, setFamily] = useState([
+    { id: 1, name: "Anna", age: 22, role: "Mother", birthday: "1956-07-25" },
+    { id: 2, name: "Jess", age: 18, role: "Daughter", birthday: "2000-07-25" }
+  ]);
 
   return (
     <>
-     <Box w='100%' p={5} >
-      <MembersTable />
-      </Box>
+      <Flex>
+        <Box w='30%' p={5} >
+          <AddMemberForm families={family} addMember={setFamily}/>
+        </Box>
+        <Box w='100%' p={5} >
+          <MembersTable families={family}/>
+        </Box>
+
+      </Flex>
     </>
   )
 }
--- src/components/MembersTable.tsx
+++ src/components/MembersTable.tsx
@@ -1,28 +1,57 @@
-import { Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
+import { DeleteIcon, EditIcon } from '@chakra-ui/icons'
+import { Button, IconButton, Img, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
 
-export const MembersTable = () => {
+interface Family {
+  name: string,
+  age: number,
+  role: string,
+  birthday: string
+}
+
+interface Props {
+  families: Family[]
+}
+export const MembersTable = ({ families }: Props) => {
   return (
     <TableContainer>
-    <Table variant='simple'>
-      <Thead>
-        <Tr>
-          <Th>Photo</Th>
-          <Th>Member Name</Th>
-          <Th>Mobile</Th>
-          <Th>Email</Th>
-          <Th>Action</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        <Tr>
-        <Td>test</Td>
-        <Td>test</Td>
-        <Td>test</Td>
-        <Td>test</Td>
-        <Td>test</Td>
-        </Tr>
-      </Tbody>
-    </Table>
-  </TableContainer>
+      <Table variant='simple'>
+        <Thead>
+          <Tr>
+            <Th>Photo</Th>
+            <Th>Member Name</Th>
+            <Th>Age</Th>
+            <Th>Role</Th>
+            <Th>Date of Birth</Th>
+            <Th>Action</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {families.map((member) =>
+            <Tr>
+              <Td><Img src='https://randomuser.me/api/portraits/women/87.jpg' boxSize='50px' borderRadius='5px'></Img></Td>
+              <Td>{member.name}</Td>
+              <Td>{member.age}</Td>
+              <Td>{member.role}</Td>
+              <Td>{member.birthday}</Td>
+              <Td>
+                <IconButton
+                  colorScheme='teal'
+                  size='sm'
+                  aria-label='Call Segun'
+                  icon={<EditIcon />}
+                />
+                  <IconButton
+                  colorScheme='red'
+                  size='sm'
+                  aria-label='Call Segun'
+                  marginX={1}
+                  icon={<DeleteIcon />}
+                />
+              </Td>
+            </Tr>
+          )}
+        </Tbody>
+      </Table>
+    </TableContainer>
   )
 }
